7cbcf732a6689cf9072484f67fbf0073c4698334
fernando.caxaj <fernando.caxaj@igssgt.org>
2022-09-28 20:43:39 -0600

    Agregando unicamente un archivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,6 +54,7 @@ import { MatTreeModule } from '@angular/material/tree';
 import { BlockCopyPasteDirective } from './directives/block-copy-paste.directive';
 import { RolesComponent } from './components/roles/roles.component';
 import { CrearRolesComponent } from './components/roles/crear-roles/crear-roles.component';
+import { RolesUsuarioComponent } from './components/roles/roles-usuario/roles-usuario.component';
 
 @NgModule({
   declarations: [
@@ -65,6 +66,7 @@ import { CrearRolesComponent } from './components/roles/crear-roles/crear-roles.
     BlockCopyPasteDirective,
     RolesComponent,
     CrearRolesComponent,
+    RolesUsuarioComponent,
   ],
   entryComponents: [],
   imports: [
